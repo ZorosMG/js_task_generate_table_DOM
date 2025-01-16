63d9f9a942ba5e7366452828cd06bb23ea986731
Denys <zorosmg5@mail.com>
2025-01-16 14:26:53 +0200

    add solutions1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -354,34 +354,37 @@ const people = [
   },
 ];
 
-
-
 const table = document.querySelector('.dashboard');
 
-people.forEach(person => {
-
+people.forEach((person) => {
   const row = document.createElement('tr');
 
   const nameCell = document.createElement('td');
+
   nameCell.textContent = person.name;
 
-  const genderCell = document.createElement('td');
-  genderCell.textContent = person.gender;
+  const sexCell = document.createElement('td');
+
+  sexCell.textContent = person.sex;
 
   const bornCell = document.createElement('td');
+
   bornCell.textContent = person.born;
 
   const diedCell = document.createElement('td');
+
   diedCell.textContent = person.died;
 
   const ageCell = document.createElement('td');
+
   ageCell.textContent = person.died - person.born;
 
   const centuryCell = document.createElement('td');
+
   centuryCell.textContent = Math.ceil(person.died / 100);
 
   row.appendChild(nameCell);
-  row.appendChild(genderCell);
+  row.appendChild(sexCell);
   row.appendChild(bornCell);
   row.appendChild(diedCell);
   row.appendChild(ageCell);
